75a2af2855743d1ddd541e21131723e3cb9cb04b
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-09 21:07:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,9 @@
-class Dog 
+class Dog
   def name=(dog_name)
-    this_dog_name = dog_name
-  end 
-  
-   
-  def name 
-    this_dog_name
-  end 
-end 
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
+end
